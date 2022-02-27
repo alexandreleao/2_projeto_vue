e0595ac156d5e5a006bb3fb5fb24b6eef85a8554
Alexandre <erdleosalgueiro@gmail.com>
2022-02-27 14:00:19 -0300

    Diretivas v-if, v-else e v-show

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
-<p>Estou trabalhando no momento.</p>
+<p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+<p v-else>Estou em busca de novas oportunidades!</p>
     <p>E uso as seguintes tecnologias:</p>
         <ul>
           <li>Javascript</li>
           <li>Php</li>
           <li>Python</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 
 <script>
   export default{
-      name: 'Info'
+      name: 'Info',
+      data(){
+        return{
+          esta_trabalhando: false,
+          mostrar_email: true,
+          email: 'alexandre@teste.com'
+        }
+      }
   }
 
 </script>
